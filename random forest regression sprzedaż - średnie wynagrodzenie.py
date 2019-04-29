7e1dc4cfc6626281d6fcd37a9ec7f67df4840837
martynadyja <48220304+martynadyja@users.noreply.github.com>
2019-04-29 13:37:18 +0200

    Add files via upload

---
+++ random forest regression sprzedaż - średnie wynagrodzenie.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+
+excelfile = pd.read_excel('dane-zestawienie.xlsx')
+columns = excelfile.iloc[:, [7, 1]]
+print(columns)
+dataset = np.array(columns)
+X = dataset[:, 0:1]
+print(X)
+Y = dataset[:, 1]
+print(Y)
+
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X, Y)
+
+Y_Pred = regressor.predict([[100]])
+print(Y_Pred)
+
+X_Grid = np.arange(min(X), max(X), 0.01)
+X_Grid = X_Grid.reshape((len(X_Grid), 1))
+plt.scatter(X, Y, color = 'r')
+plt.plot(X_Grid, regressor.predict(X_Grid), color = 'g')
+plt.title('Random Forest Regression (sprzedaż samochodów - średnie wynagrodzenie)')
+plt.xlabel('średnie wynagrodzenie')
+plt.ylabel('sprzedaż samochodów')
+plt.show()
---
+++ random forest regression sprzedaż-kurs euro.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+
+excelfile = pd.read_excel('dane-zestawienie.xlsx')
+columns = excelfile.iloc[:, [5, 1]]
+print(columns)
+dataset = np.array(columns)
+X = dataset[:, 0:1]
+print(X)
+Y = dataset[:, 1]
+print(Y)
+
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X, Y)
+
+Y_Pred = regressor.predict([[100]])
+
+X_Grid = np.arange(min(X), max(X), 0.01)
+X_Grid = X_Grid.reshape((len(X_Grid), 1))
+plt.scatter(X, Y, color = 'r')
+plt.plot(X_Grid, regressor.predict(X_Grid), color = 'g')
+plt.title('Random Forest Regression (sprzedaż samochodów - kurs euro)')
+plt.xlabel('kurs euro')
+plt.ylabel('sprzedaż samochodów')
+plt.show()
